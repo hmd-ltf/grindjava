e4ac8da969cc44f4dfc35ffc723d26adbac7b909
Hammad Latif <mhmd.ltf786@gmail.com>
2025-02-24 22:08:29 +0500

    Added basic implementation

--- phase-2/ContactManager.java
+++ phase-2/ContactManager.java
@@ -3,14 +3,14 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ContactManager {
 
-
   private List<Contact> contacts = new ArrayList<>();
 
-  public ContactManager () {
-    try (ObjectInputStream ir = new ObjectInputStream(new FileInputStream("file.txt"))) {
+  public void loadContactsFromFile(String fileName) {
+    try (ObjectInputStream ir = new ObjectInputStream(new FileInputStream(fileName))) {
       Contact contact = (Contact) ir.readObject();
       addContact(contact);
     }
@@ -34,6 +34,22 @@ public class ContactManager {
     }
 
     this.contacts.add(contact);
+  }
+
+  public void removeContactByName(String name) {
+    contacts = contacts.stream().filter(contact -> contact.name().equals(name)).collect(Collectors.toList());
+  }
+
+  public void removeContactByContact(String contact) {
+    contacts = contacts.stream().filter(c -> c.name().equals(contact)).collect(Collectors.toList());
+  }
+
+  public List<Contact> filterContactByName(String name) {
+    return contacts.stream().filter(contact -> contact.name().contains(name)).collect(Collectors.toList());
+  }
+
+  public List<Contact> filterContactByContact(String contact) {
+    return contacts.stream().filter(c -> c.name().contains(contact)).collect(Collectors.toList());
   }
 
   @Override
--- phase-2/Main.java
+++ phase-2/Main.java
@@ -1,5 +1,4 @@
 import java.io.Console;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -7,9 +6,10 @@ import java.io.ObjectOutputStream;
 public class Main {
 
   static ContactManager contactManager = new ContactManager();
+  static Console console = System.console();
 
   public static void main(String[] args) {
-    Console console = System.console();
+    contactManager.loadContactsFromFile("file.txt");
     char userInput;
 
     do {
@@ -31,17 +31,16 @@ public class Main {
 
   public static void performAction(char userInput) {
     switch (userInput) {
-      case 'A' -> createContact();
-      case 'B' -> System.out.println("Remove contact");
-      case 'C' -> System.out.println("Search contact");
-      case 'D' -> System.out.println("List contact");
+      case 'A' -> createContact(console);
+      case 'B' -> removeContact(console);
+      case 'C' -> searchContacts(console);
+      case 'D' -> printAllConacts(console);
       case '0' -> System.out.println("Exiting Application");
       default -> System.out.println("Default");
     }
   }
 
-  public static void createContact() {
-    Console console = System.console();
+  public static void createContact(Console console) {
     String contactName = console.readLine("Enter contact name: ");
     String contactNumber = console.readLine("Enter contact number: ");
 
@@ -51,9 +50,55 @@ public class Main {
     try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
       os.writeObject(contact);
     } catch (IOException e) {
-      System.out.println("Unable to code due to exception "+ e);
+      System.out.println("Unable to code due to exception " + e);
     }
 
+  }
+
+  public static void removeContact(Console console) {
+    String contactDetails;
+    boolean removeByName = false;
+    int userOption = console.readLine("""
+         A) Remove contacts by name
+         B) Remove contacts by contact
+        """).charAt(0);
+    if (userOption == 'A') {
+      removeByName = true;
+    }
+
+    contactDetails = console.readLine("Enter contact " + (removeByName ? "name: " : ": "));
+
+    if (removeByName) {
+      contactManager.removeContactByName(contactDetails);
+    } else {
+      contactManager.removeContactByContact(contactDetails);
+    }
+
+  }
+
+  public static void searchContacts(Console console) {
+    String contactDetails;
+    boolean filterByName = false;
+    int userOption = console.readLine("""
+         A) Search contacts by name
+         B) Search contacts by contact
+        """).charAt(0);
+    if (userOption == 'A') {
+      filterByName = true;
+    }
+
+    contactDetails = console.readLine("Enter contact " + (filterByName ? "name: " : ": "));
+
+    if (filterByName) {
+      console.printf("Contacts are\n" + contactManager.filterContactByName(contactDetails));
+    } else {
+      console.printf("Contacts are\n" + contactManager.filterContactByContact(contactDetails));
+    }
+
+  }
+
+  public static void printAllConacts(Console console) {
+    console.printf("Contact are\n" + contactManager);
   }
 
 }
